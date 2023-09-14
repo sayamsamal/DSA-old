cd93f302fb3505a01ca0225328070a02abcbc157
Sayam Samal <samal.sayam@gmail.com>
2023-09-14 14:31:37 +0530

    Add Subset Sums solution - using powersets.
    Link: https://practice.geeksforgeeks.org/problems/subset-sums2234/1

---
+++ Recursion/subset_sums.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    vector<int> subsetSums(vector<int> arr, int N)
+    {
+        vector<int> res;
+        for(int num=0; num<pow(2,N); num++) {
+            int sum = 0;
+            for(int i=0; i<N; i++) {
+                if(num & (1<<i)) {
+                    sum += arr[i];
+                }
+            }
+            res.push_back(sum);
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin>>N;
+        vector<int> arr(N);
+        for(int i = 0 ; i < N ; i++){
+            cin >> arr[i];
+        }
+        Solution ob;
+        vector<int> ans = ob.subsetSums(arr,N);
+        sort(ans.begin(),ans.end());
+        for(auto sum : ans){
+            cout<< sum<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
